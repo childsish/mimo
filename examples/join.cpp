11aa02853ec63e28a05ac9d44d95cd65f32e0fed
Liam Childs <Liam Childs>
2017-06-17 13:03:54 +0200

    added join example

---
+++ examples/join.cpp
@@ -0,0 +1,45 @@
+/**
+ * An example demonstrating a simple workflow that joins two streams. The outputs are never split.
+ */
+
+#include "Pipeline.h"
+#include "entities/Integer.h"
+#include "streams/Print.h"
+#include "streams/Range.h"
+
+
+class Multiply : public Stream {
+public:
+    Multiply() : Stream("multiply", {"multiplicand", "multiplier"}, {"result"}) {}
+
+    void run() {
+        Integer *multiplicand;
+        Integer *multiplier;
+        while (can_run()) {
+            multiplicand = static_cast<Integer *>(inputs["multiplicand"].pop());
+            multiplier = static_cast<Integer *>(inputs["multiplier"].pop());
+            outputs["result"].push(new Integer(multiplicand->value * multiplier->value));
+            collect(multiplicand);
+            collect(multiplier);
+        }
+    }
+};
+
+int main() {
+    Pipeline pipeline;
+    Range one_to_thousand(1, 1000, 1);
+    Range thousand_to_one(1000, 1, -1);
+    Multiply multiply;
+    Print<Integer> print;
+
+    uuid one_to_thousand_id = pipeline.add_stream(one_to_thousand);
+    uuid thousand_to_one_id = pipeline.add_stream(thousand_to_one);
+    uuid multiply_id = pipeline.add_stream(multiply);
+    uuid print_id = pipeline.add_stream(print);
+    pipeline.pipe({one_to_thousand_id, "output"}, {multiply_id, "multiplicand"});
+    pipeline.pipe({thousand_to_one_id, "output"}, {multiply_id, "multiplier"});
+    pipeline.pipe({multiply_id, "result"}, {print_id, "input"});
+    pipeline.run();
+
+    return 0;
+}
