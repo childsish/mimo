6b049012720c8f378a8a803776c0e49e0c4b88f0
Liam Childs <liam.h.childs@gmail.com>
2017-06-17 13:38:39 +0200

    added fork example

--- include/streams/Print.h
+++ include/streams/Print.h
@@ -9,16 +9,26 @@ template<typename E>
 class Print : public Stream {
 public:
 
-    Print() : Stream("print", {"input"}, {}) {}
+    Print(const std::string prefix = "", const std::string suffix = "") :
+            Stream("print", {"input"}, {}),
+            _prefix(prefix),
+            _suffix(suffix) {}
 
     void run() {
         E *entity;
         while (can_run()) {
             entity = static_cast<E *>(inputs["input"].pop());
-            std::cout << *entity << std::endl;
+            std::cout << _prefix << *entity << _suffix << std::endl;
             collect(entity);
         }
     }
+
+private:
+
+    const std::string _prefix;
+
+    const std::string _suffix;
+
 };
 
 #endif //MIMO_CAPTURE_H
