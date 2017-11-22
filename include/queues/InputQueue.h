ab7f1a77fc7cfa07dbf2eb8f454470b6a127c6f2
Liam Childs <liam.h.childs@gmail.com>
2017-11-22 10:22:28 +0100

    add queue adaptors

---
+++ include/queues/InputQueue.h
@@ -0,0 +1,34 @@
+/**
+ * @author: Liam Childs (liam.h.childs@gmail.com)
+ * @brief:
+ */
+
+#ifndef MIMO_INPUTQUEUE_H
+#define MIMO_INPUTQUEUE_H
+
+
+#include "Queue.h"
+
+namespace mimo {
+
+    /**
+     * @brief queue adaptor for queue reading functions
+     */
+    class InputQueue {
+    public:
+
+        explicit InputQueue(std::unique_ptr<Queue> queue) : _queue(std::move(queue)) {}
+
+        std::shared_ptr<mimo::Entity> peek() { return _queue->peek(); }
+        std::shared_ptr<mimo::Entity> pop() { return _queue->pop(); }
+        bool can_pop() const { return _queue->can_pop(); }
+
+    private:
+
+        std::unique_ptr<Queue> _queue;
+
+    };
+}
+
+
+#endif //MIMO_INPUTQUEUE_H
---
+++ include/queues/OutputQueue.h
@@ -0,0 +1,33 @@
+/**
+ * @author: Liam Childs (liam.h.childs@gmail.com)
+ * @brief:
+ */
+
+#ifndef MIMO_OUTPUTQUEUE_H
+#define MIMO_OUTPUTQUEUE_H
+
+
+#include "Queue.h"
+
+namespace mimo {
+
+    /**
+     * @brief queue adaptor for queue writing functions
+     */
+    class OutputQueue {
+    public:
+
+        explicit OutputQueue(std::unique_ptr<Queue> queue) : _queue(std::move(queue)) {}
+
+        bool push(std::shared_ptr<mimo::Entity> entity) { return this->_queue->push(entity); }
+        bool can_push() const { return this->_queue->can_push(); }
+
+    private:
+
+        std::unique_ptr<Queue> _queue;
+
+    };
+}
+
+
+#endif //MIMO_OUTPUTQUEUE_H
