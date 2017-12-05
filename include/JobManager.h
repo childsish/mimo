9bb963d1d52ea2c8d7b908db1a1e266162e27bd9
Liam Childs <liam.h.childs@gmail.com>
2017-12-05 07:06:09 +0100

    add empty class JobManager

---
+++ include/JobManager.h
@@ -0,0 +1,16 @@
+#ifndef MIMO_JOBMANAGER_H
+#define MIMO_JOBMANAGER_H
+
+
+namespace mimo {
+
+    /**
+     * Manages the jobs being run by the system. Prevents too many jobs from being run.
+     */
+    class JobManager {
+
+    };
+}
+
+
+#endif //MIMO_JOBMANAGER_H
