ebc3d3d2c29fad41f74d8b885e0fd01e23288dfd
Liam Childs <liam.h.childs@gmail.com>
2017-12-09 23:26:50 +0100

    remove explicit requirement from InputQueue

--- include/queues/InputQueue.h
+++ include/queues/InputQueue.h
@@ -17,7 +17,7 @@ namespace mimo {
     class InputQueue {
     public:
 
-        explicit InputQueue(std::unique_ptr<Queue> queue) : _queue(std::move(queue)) {}
+        InputQueue(std::unique_ptr<Queue> queue) : _queue(std::move(queue)) {}
 
         std::shared_ptr<mimo::Entity> peek() { return _queue->peek(); }
         std::shared_ptr<mimo::Entity> pop() { return _queue->pop(); }
