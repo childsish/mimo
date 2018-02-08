d1823022c1bb9867b2dbed3cb4713e2f423f8a86
Liam Childs <liam.h.childs@gmail.com>
2018-02-08 20:48:38 +0100

    fix Factory

--- include/Factory.h
+++ include/Factory.h
@@ -11,30 +11,31 @@
 
 namespace mimo {
 
-    template<typename T, typename ... Args>
-    class Factory : public IFactory<T, Args...> {
+    template<typename Base, typename Derived, typename ... Args>
+    class Factory : public IFactory<Base, Args...> {
 
-        T* make_raw(Args ... args) const override;
+        Base* make_raw(Args ... args) const override;
 
-        std::shared_ptr<T> make_shared(Args ... args) const override;
+        std::shared_ptr<Base> make_shared(Args ... args) const override;
 
-        std::unique_ptr<T> make_unique(Args ... args) const override;
+        std::unique_ptr<Base> make_unique(Args ... args) const override;
     };
 }
 
-template<typename T, typename ... Args>
-T* mimo::Factory<T, Args...>::make_raw(Args ... args) const {
-    return new T(std::forward<Args>(args)...);
+template<typename Base, typename Derived, typename ... Args>
+Base* mimo::Factory<Base, Derived, Args...>::make_raw(Args ... args) const {
+    return new Derived(std::forward<Args>(args)...);
 }
 
-template<typename T, typename ... Args>
-std::shared_ptr<T> mimo::Factory<T, Args...>::make_shared(Args ... args) const {
-    return std::make_shared<T>(std::forward<Args>(args)...);
+template<typename Base, typename Derived, typename ... Args>
+std::shared_ptr<Base> mimo::Factory<Base, Derived, Args...>::make_shared(Args ... args) const {
+    return std::make_shared<Derived>(std::forward<Args>(args)...);
 }
 
-template<typename T, typename ... Args>
-std::unique_ptr<T> mimo::Factory<T, Args...>::make_unique(Args ... args) const {
-    return std::make_unique<T>(std::forward<Args>(args)...);
+template<typename Base, typename Derived, typename ... Args>
+std::unique_ptr<Base> mimo::Factory<Base, Derived, Args...>::make_unique(Args ... args) const {
+    return std::make_unique<Derived>(std::forward<Args>(args)...);
 }
+
 
 #endif //MIMO_FACTORY_H
