04351630dca24510190944dcceedb7be646db7b6
Liam Childs <liam.h.childs@gmail.com>
2018-03-02 20:48:06 +0100

    fix JobInputsTest

--- tests/test_JobInputs.cpp
+++ tests/test_JobInputs.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include <workflow/Input.h>
+#include <workflow/Step.h>
+#include <workflow/Workflow.h>
 #include <Entity.h>
 #include <errors.h>
 #include "mocks/MockQueue.h"
@@ -11,86 +13,83 @@
 using ::testing::Return;
 
 TEST(JobInputsTest, test_get_status_two_full_queues) {
-    workflow::InputMap inputs({
-        {"queue1", std::make_shared<workflow::Input>("queue1")},
-        {"queue2", std::make_shared<workflow::Input>("queue2")}
-    });
+    workflow::Workflow workflow;
+    auto step = workflow.add_step("step", {"input1", "input2"}, {});
+    auto inputs = step->get_inputs();
     auto factory = std::make_shared<mimo::MockQueueFactory>();
-    auto *queue1 = new mimo::MockQueue();
-    auto *queue2 = new mimo::MockQueue();
+    auto *input1 = new mimo::MockQueue();
+    auto *input2 = new mimo::MockQueue();
 
     EXPECT_CALL(*factory, make_unique_proxy())
-        .WillOnce(Return(queue1))
-        .WillOnce(Return(queue2));
+        .WillOnce(Return(input1))
+        .WillOnce(Return(input2));
 
-    EXPECT_CALL(*queue1, can_pop())
+    EXPECT_CALL(*input1, can_pop())
         .WillRepeatedly(Return(true));
-    EXPECT_CALL(*queue2, can_pop())
+    EXPECT_CALL(*input2, can_pop())
         .WillRepeatedly(Return(true));
 
     mimo::JobInputs job_inputs(inputs, factory);
     EXPECT_EQ(job_inputs.get_status(), mimo::JobInputs::PopStatus::CAN_POP);
-    EXPECT_EQ(job_inputs.get_status("queue1"), mimo::JobInputs::PopStatus::CAN_POP);
-    EXPECT_EQ(job_inputs.get_status("queue2"), mimo::JobInputs::PopStatus::CAN_POP);
-    inputs["queue1"]->sync_group = 1;
+    EXPECT_EQ(job_inputs.get_status("input1"), mimo::JobInputs::PopStatus::CAN_POP);
+    EXPECT_EQ(job_inputs.get_status("input2"), mimo::JobInputs::PopStatus::CAN_POP);
+    inputs["input1"]->sync_group = 1;
     EXPECT_EQ(job_inputs.get_status(), mimo::JobInputs::PopStatus::CAN_POP);
-    EXPECT_EQ(job_inputs.get_status("queue1"), mimo::JobInputs::PopStatus::CAN_POP);
-    EXPECT_EQ(job_inputs.get_status("queue2"), mimo::JobInputs::PopStatus::CAN_POP);
+    EXPECT_EQ(job_inputs.get_status("input1"), mimo::JobInputs::PopStatus::CAN_POP);
+    EXPECT_EQ(job_inputs.get_status("input2"), mimo::JobInputs::PopStatus::CAN_POP);
 }
 
 TEST(JobInputsTest, test_get_status_one_full_one_empty_queue) {
-    workflow::InputMap inputs({
-        {"queue1", std::make_shared<workflow::Input>("queue1")},
-        {"queue2", std::make_shared<workflow::Input>("queue2")}
-    });
+    workflow::Workflow workflow;
+    auto step = workflow.add_step("step", {"input1", "input2"}, {});
+    auto inputs = step->get_inputs();
     auto factory = std::make_shared<mimo::MockQueueFactory>();
-    auto *queue1 = new mimo::MockQueue();
-    auto *queue2 = new mimo::MockQueue();
+    auto *input1 = new mimo::MockQueue();
+    auto *input2 = new mimo::MockQueue();
 
     EXPECT_CALL(*factory, make_unique_proxy())
-        .WillOnce(Return(queue1))
-        .WillOnce(Return(queue2));
+        .WillOnce(Return(input1))
+        .WillOnce(Return(input2));
 
-    bool queue1_return_value = inputs.begin()->first == "queue1";
-    EXPECT_CALL(*queue1, can_pop())
-        .WillRepeatedly(Return(queue1_return_value));
-    EXPECT_CALL(*queue2, can_pop())
-        .WillRepeatedly(Return(!queue1_return_value));
+    bool input1_return_value = inputs.begin()->first == "input1";
+    EXPECT_CALL(*input1, can_pop())
+        .WillRepeatedly(Return(input1_return_value));
+    EXPECT_CALL(*input2, can_pop())
+        .WillRepeatedly(Return(!input1_return_value));
 
     mimo::JobInputs job_inputs(inputs, factory);
     EXPECT_EQ(job_inputs.get_status(), mimo::JobInputs::PopStatus::SYNC_QUEUE_EMPTY);
-    EXPECT_EQ(job_inputs.get_status("queue1"), mimo::JobInputs::PopStatus::SYNC_QUEUE_EMPTY);
-    EXPECT_EQ(job_inputs.get_status("queue2"), mimo::JobInputs::PopStatus::QUEUE_EMPTY);
-    inputs["queue1"]->sync_group = 1;
+    EXPECT_EQ(job_inputs.get_status("input1"), mimo::JobInputs::PopStatus::SYNC_QUEUE_EMPTY);
+    EXPECT_EQ(job_inputs.get_status("input2"), mimo::JobInputs::PopStatus::QUEUE_EMPTY);
+    inputs["input1"]->sync_group = 1;
     EXPECT_EQ(job_inputs.get_status(), mimo::JobInputs::PopStatus::CAN_POP);
-    EXPECT_EQ(job_inputs.get_status("queue1"), mimo::JobInputs::PopStatus::CAN_POP);
-    EXPECT_EQ(job_inputs.get_status("queue2"), mimo::JobInputs::PopStatus::QUEUE_EMPTY);
+    EXPECT_EQ(job_inputs.get_status("input1"), mimo::JobInputs::PopStatus::CAN_POP);
+    EXPECT_EQ(job_inputs.get_status("input2"), mimo::JobInputs::PopStatus::QUEUE_EMPTY);
 }
 
 TEST(JobInputsTest, test_get_status_two_empty_queues) {
-    workflow::InputMap inputs({
-        {"queue1", std::make_shared<workflow::Input>("queue1")},
-        {"queue2", std::make_shared<workflow::Input>("queue2")}
-    });
+    workflow::Workflow workflow;
+    auto step = workflow.add_step("step", {"input1", "input2"}, {});
+    auto inputs = step->get_inputs();
     auto factory = std::make_shared<mimo::MockQueueFactory>();
-    auto *queue1 = new mimo::MockQueue();
-    auto *queue2 = new mimo::MockQueue();
+    auto *input1 = new mimo::MockQueue();
+    auto *input2 = new mimo::MockQueue();
 
     EXPECT_CALL(*factory, make_unique_proxy())
-        .WillOnce(Return(queue1))
-        .WillOnce(Return(queue2));
+        .WillOnce(Return(input1))
+        .WillOnce(Return(input2));
 
-    EXPECT_CALL(*queue1, can_pop())
+    EXPECT_CALL(*input1, can_pop())
         .WillRepeatedly(Return(false));
-    EXPECT_CALL(*queue2, can_pop())
+    EXPECT_CALL(*input2, can_pop())
         .WillRepeatedly(Return(false));
 
     mimo::JobInputs job_inputs(inputs, factory);
     EXPECT_EQ(job_inputs.get_status(), mimo::JobInputs::PopStatus::SYNC_QUEUE_EMPTY);
-    EXPECT_EQ(job_inputs.get_status("queue1"), mimo::JobInputs::PopStatus::QUEUE_EMPTY);
-    EXPECT_EQ(job_inputs.get_status("queue2"), mimo::JobInputs::PopStatus::QUEUE_EMPTY);
-    inputs["queue1"]->sync_group = 1;
+    EXPECT_EQ(job_inputs.get_status("input1"), mimo::JobInputs::PopStatus::QUEUE_EMPTY);
+    EXPECT_EQ(job_inputs.get_status("input2"), mimo::JobInputs::PopStatus::QUEUE_EMPTY);
+    inputs["input1"]->sync_group = 1;
     EXPECT_EQ(job_inputs.get_status(), mimo::JobInputs::PopStatus::SYNC_QUEUE_EMPTY);
-    EXPECT_EQ(job_inputs.get_status("queue1"), mimo::JobInputs::PopStatus::QUEUE_EMPTY);
-    EXPECT_EQ(job_inputs.get_status("queue2"), mimo::JobInputs::PopStatus::QUEUE_EMPTY);
+    EXPECT_EQ(job_inputs.get_status("input1"), mimo::JobInputs::PopStatus::QUEUE_EMPTY);
+    EXPECT_EQ(job_inputs.get_status("input2"), mimo::JobInputs::PopStatus::QUEUE_EMPTY);
 }
