ee2ed66a31e7d3892921c895f8038011c6066aef
Liam Childs <liam.h.childs@gmail.com>
2018-03-14 21:39:03 +0100

    improve AsynchronousJobManagerTest

--- tests/test_AsynchronousJobManager.cpp
+++ tests/test_AsynchronousJobManager.cpp
@@ -10,20 +10,33 @@
 
 using ::testing::Return;
 
+TEST(AsynchronousJobManagerTest, test_empty_job_not_runnable) {
+    workflow::Workflow workflow;
+
+    auto job_proxy = new mimo::MockJob();
+    EXPECT_CALL(*job_proxy, can_run())
+        .WillRepeatedly(Return(false));
+
+    auto factory = std::make_shared<mimo::MockJobFactory>();
+    auto identifier = workflow.add_step("step1", {}, {});
+    std::shared_ptr<mimo::Step> step = std::make_shared<mimo::MockStep>();
+    EXPECT_CALL(*factory, make_shared_proxy(identifier, step))
+        .WillOnce(Return(job_proxy));
+
+    mimo::AsynchronousJobManager manager(identifier, step, factory);
+
+    EXPECT_FALSE(manager.has_runnable_job());
+}
+
 TEST(AsynchronousJobManagerTest, test_only_one_job_allowed) {
     workflow::Workflow workflow;
 
     auto job_proxy = new mimo::MockJob();
     EXPECT_CALL(*job_proxy, can_run())
-        .WillOnce(Return(false))
-        .WillOnce(Return(true))
-        .WillOnce(Return(true))
-        .WillOnce(Return(true));
+        .WillRepeatedly(Return(true));
     auto identifier = workflow.add_step("step1", {}, {});
     EXPECT_CALL(*job_proxy, get_step_id())
-        .WillOnce(Return(identifier))
-        .WillOnce(Return(identifier))
-        .WillOnce(Return(identifier));
+        .WillRepeatedly(Return(identifier));
 
     auto factory = std::make_shared<mimo::MockJobFactory>();
     std::shared_ptr<mimo::Step> step = std::make_shared<mimo::MockStep>();
@@ -32,17 +45,36 @@ TEST(AsynchronousJobManagerTest, test_only_one_job_allowed) {
 
     mimo::AsynchronousJobManager manager(identifier, step, factory);
 
-    EXPECT_FALSE(manager.has_runnable_job());
     EXPECT_TRUE(manager.has_runnable_job());
     auto job = manager.get_runnable_job();
     EXPECT_FALSE(manager.has_runnable_job());
+    manager.return_complete_job(job);
+    EXPECT_TRUE(manager.has_runnable_job());
+}
+
+TEST(AsynchronousJobManagerTest, test_return_wrong_job) {
+    workflow::Workflow workflow;
+
+    auto job_proxy = new mimo::MockJob();
+    EXPECT_CALL(*job_proxy, can_run())
+        .WillRepeatedly(Return(true));
+    auto identifier = workflow.add_step("step1", {}, {});
+    EXPECT_CALL(*job_proxy, get_step_id())
+        .WillRepeatedly(Return(identifier));
+
+    auto factory = std::make_shared<mimo::MockJobFactory>();
+    std::shared_ptr<mimo::Step> step = std::make_shared<mimo::MockStep>();
+    EXPECT_CALL(*factory, make_shared_proxy(identifier, step))
+        .WillOnce(Return(job_proxy));
+
+    mimo::AsynchronousJobManager manager(identifier, step, factory);
 
     auto wrong_job = std::make_shared<mimo::MockJob>();
     auto wrong_identifier = workflow.add_step("step2", {}, {});
     EXPECT_CALL(*wrong_job, get_step_id())
-        .WillOnce(Return(wrong_identifier));
+        .WillRepeatedly(Return(wrong_identifier));
     EXPECT_THROW(manager.return_complete_job(wrong_job), std::runtime_error);
 
-    manager.return_complete_job(job);
-    EXPECT_TRUE(manager.has_runnable_job());
+    auto job = manager.get_runnable_job();
+    EXPECT_THROW(manager.return_complete_job(wrong_job), std::runtime_error);
 }
