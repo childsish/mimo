71bc4c14516febe3cbc9242a3ed5c13b3faae05f
Liam Childs <liam.h.childs@gmail.com>
2018-06-07 14:04:19 +0200

    update MockQueueBundle

--- tests/mocks/MockQueueBundle.h
+++ tests/mocks/MockQueueBundle.h
@@ -10,11 +10,8 @@
 namespace mimo {
     class MockQueueBundle : public IQueueBundle {
     public:
-        void acquire_queue(const workflow::Connection &connection, std::unique_ptr<IQueue> queue) {
-            this->acquire_queue_proxy(connection, queue.get());
-        }
-
-
+        MOCK_CONST_METHOD0(get_identifiers, const ConnectionMap &());
+        MOCK_METHOD1(get_queue, IQueue &(const std::string &));
         MOCK_CONST_METHOD0(get_pop_status, PopStatus());
         MOCK_CONST_METHOD1(get_pop_status, PopStatus(const std::string &name));
         MOCK_METHOD1(peek, std::shared_ptr<Entity>(const std::string &name));
@@ -22,10 +19,6 @@ namespace mimo {
         MOCK_CONST_METHOD0(get_push_status, PushStatus());
         MOCK_CONST_METHOD1(get_push_status, PushStatus(const std::string &));
         MOCK_METHOD2(push, void(const std::string &, std::shared_ptr<Entity>));
-        MOCK_CONST_METHOD0(get_identifiers, const ConnectionMap &());
-        MOCK_METHOD1(release_queue, std::unique_ptr<IQueue>(const workflow::Connection &));
-        MOCK_METHOD2(acquire_queue_proxy, void(const workflow::Connection &, IQueue *));
-        MOCK_METHOD2(push, void(const std::string &, const IQueue &));
     };
 }
 
