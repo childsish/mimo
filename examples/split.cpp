cf89b632a59e38b7f46e2977733655667566a0e8
Liam Childs <liam.h.childs@gmail.com>
2018-07-05 22:26:35 +0200

    fix split test

--- examples/split.cpp
+++ examples/split.cpp
@@ -2,26 +2,29 @@
  * An example demonstrating a simple workflow That splits the output of a stream.
  */
 
-#include "Pipeline.h"
-#include "entities/Integer.h"
-#include "streams/Print.h"
-#include "streams/Range.h"
+#include <workflow/Workflow.h>
+#include <mimo/Engine.h>
+#include "steps/Print.h"
+#include "steps/Range.h"
 
 
 int main() {
-    Queue::THRESHOLD = 2;
+    auto range = std::make_shared<Range>(0, 100000, 1);
+    auto print_left = std::make_shared<Print<Integer>>("", " was split left.");
+    auto print_right = std::make_shared<Print<Integer>>("", " was split right.");
 
-    Pipeline pipeline;
-    Range range(10);
-    Print<Integer> print_left("", " was split left.");
-    Print<Integer> print_right("", " was split right.");
+    auto workflow = std::make_shared<workflow::Workflow>();
+    auto range_step = workflow->add_step(range->get_name(), range->get_inputs(), range->get_outputs());
+    auto print_left_step = workflow->add_step(print_left->get_name(), print_left->get_inputs(), print_left->get_outputs());
+    auto print_right_step = workflow->add_step(print_right->get_name(), print_right->get_inputs(), print_right->get_outputs());
+    range_step->pipe(print_left_step);
+    range_step->pipe(print_right_step);
 
-    uuid range_id = pipeline.add_stream(range);
-    uuid print_left_id = pipeline.add_stream(print_left);
-    uuid print_right_id = pipeline.add_stream(print_right);
-    pipeline.pipe({range_id, "output"}, {print_left_id, "input"});
-    pipeline.pipe({range_id, "output"}, {print_right_id, "input"});
-    pipeline.run();
+    mimo::Engine engine;
+    engine.register_step(range_step, range);
+    engine.register_step(print_left_step, print_left);
+    engine.register_step(print_right_step, print_right);
+    engine.run(workflow);
 
     return 0;
 }
